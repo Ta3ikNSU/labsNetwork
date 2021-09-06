6c647592174530bedf78e4793c415d9c32b53acc
Ta3ik <snoopliner@gmail.com>
2021-09-07 00:22:42 +0700

    tret

--- lab1/FinderOfCopy.java
+++ lab1/FinderOfCopy.java
@@ -1,28 +1,36 @@
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class FinderOfCopy {
-    public static final int PORT = 10000;
+    public final int PORT = 10000;
+    private DatagramSocket datagramSocket = null;
+    private MulticastSocket multicastSocket = null;
+    private byte[] buf = new byte[256];
+    private String myIP;
+    private InetAddress inetAddress;
 
-    private static DatagramSocket datagramSocket = null;
-    private static MulticastSocket multicastSocket = null;
-    private static byte[] buf = new byte[256];
-    private static String myIP;
-    private static InetAddress inetAddress;
-    public static void find(String ip) throws Exception {
+    public FinderOfCopy(String ip) throws Exception {
+        find(ip);
+    }
+    public void find(String ip) throws Exception {
+//        System.out.println(ProcessHandle.current().pid());
         myIP = InetAddress.getLocalHost().getHostAddress();
-        System.out.println(myIP);
+//        System.out.println(myIP);
         inetAddress = InetAddress.getByName(ip);
         datagramSocket = new DatagramSocket();
         multicastSocket = new MulticastSocket(PORT);
         multicastSocket.joinGroup(inetAddress);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("finalize");
-            String sendBuf = "DISCONNECT " + myIP;
+//            System.out.println("finalize");
+            String sendBuf = "DISCONNECT " + myIP + " " + ProcessHandle.current().pid();
             DatagramPacket packet = new DatagramPacket(sendBuf.getBytes(StandardCharsets.UTF_8), sendBuf.length(), inetAddress, PORT);
             try {
                 datagramSocket.send(packet);
@@ -33,59 +41,63 @@ public class FinderOfCopy {
         start(inetAddress);
     }
 
-    private static void start(InetAddress inetAddress) throws IOException {
+    private void start(InetAddress inetAddress) throws IOException {
         connect();
         datagramSocket.setBroadcast(true);
-        System.out.println("connect msg has been sended");
+//        System.out.println("connect msg has been sent");
         while (true) {
             receiveMsg();
-            System.out.println("msg was recieve");
+//            System.out.println("msg was receive");
         }
     }
 
-    private static void connect() throws IOException {
-        String sendBuf = "CONNECT " + myIP;
+    private void connect() throws IOException {
+        String sendBuf = "CONNECT " + myIP + " " + ProcessHandle.current().pid();
         DatagramPacket packet = new DatagramPacket(sendBuf.getBytes(StandardCharsets.UTF_8), sendBuf.length(), inetAddress, PORT);
         datagramSocket.send(packet);
     }
 
 
-    private static void receiveMsg() throws IOException {
+    private void receiveMsg() throws IOException {
         DatagramPacket packet = new DatagramPacket(buf, buf.length);
         multicastSocket.receive(packet);
         msgHandler(packet);
     }
 
 
-    public static void msgHandler(DatagramPacket packet) {
+    public void msgHandler(DatagramPacket packet) throws IOException {
         String[] dataArgs = new String(packet.getData(), 0, packet.getLength()).split(" +");
+//        System.out.println(dataArgs[2]);
         switch (dataArgs[0]) {
             case "CONNECT": {
-                System.out.println(" + " + dataArgs[1]);
-                answer();
+                if (!dataArgs[2].equals(String.valueOf(ProcessHandle.current().pid()))) {
+                    System.out.println(" + " + dataArgs[1] + " " + dataArgs[2]);
+                    answer();
+                }
+
+                break;
+            }
+            case "OLD_MACHINE": {
+                if (!dataArgs[2].equals(String.valueOf(ProcessHandle.current().pid()))) {
+                    System.out.println(" * " + dataArgs[1]+ " " + dataArgs[2]);
+                }
+
                 break;
             }
             case "DISCONNECT": {
-                System.out.println(" - " + dataArgs[1]);
+                if (!dataArgs[2].equals(String.valueOf(ProcessHandle.current().pid()))) {
+                    System.out.println(" - " + dataArgs[1] + " " + dataArgs[2]);
+                }
+
                 break;
             }
         }
     }
 
-    public static void answer() {
-        String sendBuf = "OLD_MACHINE " + myIP;
+    public void answer() throws IOException {
+        String sendBuf = "OLD_MACHINE " + myIP + " " + ProcessHandle.current().pid();
         DatagramPacket packet = new DatagramPacket(sendBuf.getBytes(StandardCharsets.UTF_8), sendBuf.length(), inetAddress, PORT);
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        try {
-            System.out.println("finalize");
-            String sendBuf = "DISCONNECT " + myIP;
-            DatagramPacket packet = new DatagramPacket(sendBuf.getBytes(StandardCharsets.UTF_8), sendBuf.length(), inetAddress, PORT);
-        } finally {
-            super.finalize();
-        }
+        datagramSocket.send(packet);
     }
 
     private enum InstanceMsg {
